e74b77d62c46af87387664578f85385125197593
si2022 <pedrohenriquedejesusferreira13@gmail.com>
2022-09-05 09:46:54 -0300

    teste1

--- src/Jogo.java
+++ src/Jogo.java
@@ -15,7 +15,7 @@ public class Jogo{
 
         System.out.println(lin+tit+lin+"\n|\t\t\t   "+jo+es+aj+ess+cr+sr+"\t\t\t\t|"+lin);
 
-
+/*nós somos tops o resto são sutians*/
     }public static void controlemenu() {//controla as acões do menu
         String selecionarop="x",lin ="|-----------------------------------|";
         Scanner input= new Scanner(System.in);
